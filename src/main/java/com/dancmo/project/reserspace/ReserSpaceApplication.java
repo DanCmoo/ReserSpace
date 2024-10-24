7a722ac7248d61cf0a780adea137db9ee817bd38
DanCmo <dankurso845@gmail.com>
2024-10-23 19:07:46 -0500

    initial commit

---
+++ src/main/java/com/dancmo/project/reserspace/ReserSpaceApplication.java
@@ -0,0 +1,13 @@
+package com.dancmo.project.reserspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserSpaceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReserSpaceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dancmo/project/reserspace/ReserSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dancmo.project.reserspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReserSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
